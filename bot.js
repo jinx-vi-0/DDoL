e1500a4569290cfe9cce4a629f1af249a683c11a
Adarsh <adarshkumarmsay@gmail.com>
2024-07-12 13:14:28 +0530

    Added /random command

--- bot.js
+++ bot.js
@@ -1,8 +1,11 @@
-const { Client, GatewayIntentBits, Partials } = require('discord.js');
-const { LeetCode } = require('leetcode-query');
-const cron = require('node-cron');
-const keep_alive = require('./keep_alive.js')
-require('dotenv').config();
+import { Client, GatewayIntentBits, Partials } from 'discord.js';
+import { LeetCode } from 'leetcode-query';
+import cron from 'node-cron';
+import keep_alive from './keep_alive.js';
+import dotenv from 'dotenv';
+import leetcodeProblems from './problems.js';
+
+dotenv.config();
 
 const client = new Client({
     intents: [
@@ -20,13 +23,13 @@ const lc = new LeetCode();
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 
-    cron.schedule('0 7 * * *', async () => {
+    cron.schedule('0 6 * * *', async () => {
         try {
             const daily = await lc.daily();
             const channel = client.channels.cache.get(process.env.CHANNEL_ID);
             if (channel) {
                 const questionLink = `https://leetcode.com${daily.link}`;
-                const response = `@everyone **LeetCode Daily Challenge Question for ${daily.date}:**\n**${daily.question.title}** : ${questionLink}`;
+                const response = `@everyone **LeetCode Daily Challenge ${daily.date}:**\n**${daily.question.title}** : ${questionLink}`;
                 channel.send(response);
             } else {
                 console.error('Channel not found');
@@ -41,15 +44,28 @@ client.once('ready', () => {
 });
 
 client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
     if (message.content === '/potd') {
         try {
             const daily = await lc.daily();
             const questionLink = `https://leetcode.com${daily.link}`;
-            const responseMessage = `@everyone **LeetCode Daily Challenge Question for ${daily.date}:**\n**${daily.question.title}** : ${questionLink}`;
+            const responseMessage = `**LeetCode Daily Challenge ${daily.date}:**\n**${daily.question.title}** : ${questionLink}`;
             message.channel.send(responseMessage);
         } catch (error) {
             console.error('Error fetching LeetCode daily challenge:', error);
             message.channel.send('Sorry, I could not fetch the LeetCode daily challenge question.');
+        }
+    } else if (message.content === '/random') {
+        try {
+            const randomIndex = Math.floor(Math.random() * leetcodeProblems.length);
+            let problem = leetcodeProblems[randomIndex];
+            let selectedProblem = problem.toLowerCase().replace(/ /g, '-');
+            const questionLink = `https://leetcode.com/problems/${selectedProblem}`;
+            const responseMessage = `**Problem:** **${problem}** :\n ${questionLink}`;
+            message.channel.send(responseMessage);
+        } catch (error) {
+            console.error('Error fetching random LeetCode question:', error);
+            message.channel.send('Sorry, I could not fetch a random LeetCode question.');
         }
     }
 });
--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,10 @@
-const http = require('http')
+import http from 'http';
 
-http.createServer((req, res) => {
-    res.write("I'm alive")
-    res.end()
-}).listen(8080)
+export default function keepAlive() {
+    http.createServer((req, res) => {
+        res.write("I'm alive");
+        res.end();
+    }).listen(8080);
+
+    console.log("Server is running on port 8080");
+}
