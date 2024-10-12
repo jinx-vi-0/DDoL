88f6a4889500ba9ae83ffb1c5e46b01ab58646d1
rishabh7923 <rishabhyadav7923@gmail.com>
2024-10-12 23:15:28 +0530

    Updated Daily Leetcode question

--- events/ready.js
+++ events/ready.js
@@ -1,24 +1,32 @@
+import { EmbedBuilder, bold } from "discord.js";
 import cron from 'node-cron'
 
-export default async (client, lc=client.lc) => {
+export default async (client, lc = client.lc) => {
     console.log(`Logged in as ${client.user.tag}!`);
 
     cron.schedule('0 6 * * *', async () => {
-        try {
-            const daily = await lc.daily();
-            const channel = client.channels.cache.get(process.env.CHANNEL_ID);
-            if (channel) {
-                const questionLink = `https://leetcode.com${daily.link}`;
-                const response = `@everyone **LeetCode Daily Challenge ${daily.date}:**\n**${daily.question.title}** : ${questionLink}`;
-                channel.send(response);
-            } else {
-                console.error('Channel not found');
-            }
-        } catch (error) {
-            console.error('Error fetching LeetCode daily challenge:', error);
-        }
+        const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+        if (!channel) return console.error(`Failed to find channel for daily challenge (${channel})`)
+
+        const { question, link, date } = await lc.daily();
+
+        const embed = new EmbedBuilder()
+            .setTitle(question.title)
+            .setURL(`https://leetcode.com${link}`)
+            .setColor(0xfea116)
+            .addFields(
+                { name: 'Difficulty', value: question.difficulty, inline: true },
+                { name: 'Link', value: `[Click Here](https://leetcode.com${link})`, inline: true },
+                /* Show 3 topics in each line for better formatting */
+                { name: 'Topic', value: question.topicTags.map((x, i) => (i % 3 === 0 && i !== 0 ? '\n' : '') + x.name).join(', ') }
+            )
+
+        return channel.send({
+            content: bold(`@everyone, LeetCode Daily Challenge: \`${date}\``),
+            embeds: [embed]
+        })
     }, {
         scheduled: true,
         timezone: "Asia/Kolkata"
-    });
+    })
 }
